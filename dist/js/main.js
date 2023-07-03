f09b277f4a7ef14411d29284f1acb3c23f72a508
strophy <badfunkstripe@gmail.com>
2023-07-03 18:51:52 +1000

    fix: js formatting

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,26 +1,26 @@
-  (function($) { 
-  $(function() { 
+(function ($) {
+  $(function () {
     //  open and close nav 
-    $('#navbar-toggle').click(function() {
+    $('#navbar-toggle').click(function () {
       $('nav ul.nav-list').slideToggle();
     });
     // Hamburger toggle
-    $('#navbar-toggle').on('click', function() {
+    $('#navbar-toggle').on('click', function () {
       this.classList.toggle('active');
     });
-  }); 
-})(jQuery); 
+  });
+})(jQuery);
 // -- YouTube/Vimeo Frame Mobile Friendly --//
-$(function() {
+$(function () {
   var $allVideos = $("iframe[src*='www.youtube.com'], iframe[src*='player.vimeo.com']");
-  $allVideos.each(function() {
+  $allVideos.each(function () {
     $(this)
       .data('aspectRatio', this.height / this.width)
       .removeAttr('height')
       .removeAttr('width');
   });
-  $(window).resize(function() {
-    $allVideos.each(function() {
+  $(window).resize(function () {
+    $allVideos.each(function () {
       var $el = $(this);
       var newWidth = $el.parent().width();
       $el
@@ -30,8 +30,8 @@ $(function() {
   }).resize();
 });
 // -- Mobile Button --//
-$(document).ready(function(){
-  $("parent").click(function(){
-  $("ul").toggleClass("child");
+$(document).ready(function () {
+  $("parent").click(function () {
+    $("ul").toggleClass("child");
   });
 });
